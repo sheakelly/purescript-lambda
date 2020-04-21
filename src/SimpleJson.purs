7fd48642daf304e27a43070b9c79714791ed5c19
Shea Kelly <shea.kelly@lwb.org.au>
2020-04-21 14:53:40 +1000

    trying out purty

--- src/SimpleJson.purs
+++ src/SimpleJson.purs
@@ -1,7 +1,6 @@
 module SimpleJson where
 
 import Prelude
-
 import Control.Monad.Except (throwError)
 import Effect (Effect)
 import Effect.Class (liftEffect)
@@ -13,22 +12,23 @@ import Effect.Exception (error)
 import Data.Either (Either(..))
 import Simple.JSON (class ReadForeign, read, write)
 
-type Input = { id :: Number, message :: String }
+type Input
+  = { id :: Number, message :: String }
 
-type Output = { text :: String }
+type Output
+  = { text :: String }
 
 withEvent :: forall a. ReadForeign a => Foreign -> (a -> Foreign) -> Effect (Promise Foreign)
-withEvent f fn =
-  case read f of
-    Right (r :: a) ->
-      liftEffect $ fromAff $ pure $ fn r
-    Left e ->
-      liftEffect $ fromAff $ throwError $ error $ show e
-
+withEvent f fn = case read f of
+  Right (r :: a) -> liftEffect $ fromAff $ pure $ fn r
+  Left e -> liftEffect $ fromAff $ throwError $ error $ show e
 
 handler' :: Foreign -> Effect (Promise Foreign)
-handler' f = withEvent f (\(input :: Input)
-                            -> write { text: "You said: " <> input.message })
+handler' f =
+  withEvent f
+    ( \(input :: Input) ->
+        write { text: "You said: " <> input.message }
+    )
 
 handler :: EffectFn1 Foreign (Promise Foreign)
 handler = mkEffectFn1 handler'
