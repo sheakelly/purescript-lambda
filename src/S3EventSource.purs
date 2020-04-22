ef7da35afcd7ba828cd02cd3f4a900e84a6b2f09
Shea Kelly <shea.kelly@lwb.org.au>
2020-04-22 16:04:40 +1000

    parsing s3 event correctly

--- src/S3EventSource.purs
+++ src/S3EventSource.purs
@@ -12,32 +12,49 @@ import Data.Either (Either(..))
 import Data.Traversable (traverse)
 import Foreign (F, Foreign, MultipleErrors, readString, readArray, unsafeToForeign, renderForeignError)
 import Foreign.Index ((!))
+import Debug.Trace (traceM)
+
+readS3Object :: Foreign -> F S3Object
+readS3Object value = do
+  key <- value ! "key" >>= readString
+  pure { key }
 
 readS3Bucket :: Foreign -> F S3Bucket
 readS3Bucket value = do
   name <- value ! "name" >>= readString
-  arn <- value ! "arn" >>= readString
-  pure { name, arn }
+  pure { name }
 
-readS3EventRecord :: Foreign -> F S3EventRecord
-readS3EventRecord value = do
-  s3 <- value ! "s3" >>= readS3Bucket
+readS3 :: Foreign -> F S3
+readS3 value = do
+  bucket <- value ! "bucket" >>= readS3Bucket
+  object <- value ! "object" >>= readS3Object
+  pure { bucket, object }
+
+readS3Record :: Foreign -> F S3Record
+readS3Record value = do
+  s3 <- value ! "s3" >>= readS3
   pure { s3 }
 
 readS3Event :: Foreign -> F S3Event
 readS3Event value = do
-  records <- value ! "Records" >>= readArray >>= traverse readS3EventRecord
+  records <- value ! "Records" >>= readArray >>= traverse readS3Record
   pure { records }
 
 -- Not including all the fields for now
-type S3Bucket
-  = { name :: String, arn :: String }
+type S3Object
+  = { key :: String }
 
-type S3EventRecord
-  = { s3 :: S3Bucket }
+type S3Bucket
+  = { name :: String }
+
+type S3
+  = { bucket :: S3Bucket, object :: S3Object }
+
+type S3Record
+  = { s3 :: S3 }
 
 type S3Event
-  = { records :: Array S3EventRecord }
+  = { records :: Array S3Record }
 
 renderForeignErrors :: MultipleErrors -> String
 renderForeignErrors e =
@@ -49,10 +66,11 @@ handleEvent value reader fn = case (runExcept $ reader value) of
   Left e -> liftEffect $ fromAff $ throwError $ error $ renderForeignErrors e
 
 handler' :: Foreign -> Effect (Promise Foreign)
-handler' event =
+handler' event = do
+  traceM event
   handleEvent event readS3Event
     ( \s3Event ->
-        { text: "s3 events: " <> show s3Event.records }
+        "s3 events: " <> show s3Event.records
     )
 
 handler :: EffectFn1 Foreign (Promise Foreign)
--- test.js
+++ test.js
@@ -2,8 +2,44 @@ const { handler } = require("./output/S3EventSource");
 
 // handler({ id: 1, message: "Yo Dawg" }).then((r) => console.log(r));
 
-handler({ id2: 1, text: "Yo" }).then((r) => console.log(r));
+//handler({ id2: 1, text: "Yo" }).then((r) => console.log(r));
 
 handler({
-  Records: [{ s3: { name: "bucket name", arn: "arn value" } }],
+  Records: [
+    {
+      eventVersion: "2.0",
+      eventSource: "aws:s3",
+      awsRegion: "ap-southeast-2",
+      eventTime: "1970-01-01T00:00:00.000Z",
+      eventName: "ObjectCreated:Put",
+      userIdentity: {
+        principalId: "EXAMPLE",
+      },
+      requestParameters: {
+        sourceIPAddress: "127.0.0.1",
+      },
+      responseElements: {
+        "x-amz-request-id": "EXAMPLE123456789",
+        "x-amz-id-2":
+          "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH",
+      },
+      s3: {
+        s3SchemaVersion: "1.0",
+        configurationId: "testConfigRule",
+        bucket: {
+          name: "example-bucket",
+          ownerIdentity: {
+            principalId: "EXAMPLE",
+          },
+          arn: "arn:aws:s3:::example-bucket",
+        },
+        object: {
+          key: "test/key",
+          size: 1024,
+          eTag: "0123456789abcdef0123456789abcdef",
+          sequencer: "0A1B2C3D4E5F678901",
+        },
+      },
+    },
+  ],
 }).then((r) => console.log(r));
